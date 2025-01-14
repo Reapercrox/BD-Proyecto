f7fe4de81eeee1758a736c744e04a3a777928822
Reapercrox <memisguiller202@gmail.com>
2025-01-14 00:32:23 -0600

    Se logro solucionar el problema de dependencias con java, se añadio el codigo de conexion de base de datos

--- src/main/java/Database/Database_connection.java
+++ src/main/java/Database/Database_connection.java
@@ -6,25 +6,50 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
-/*import oracle.jdbc.internal.OracleTypes;*/
+import oracle.jdbc.internal.OracleTypes;
 
-/**
- *
- * @author memis
- */
 public class Database_connection {
-    
-    /*private static Connection getConnection() {
+
+    /**
+     * Este método retorna un objeto de conexión a la base de datos. 
+     * @return 
+     */
+    private static Connection getConnection() {
         try {
             DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
             // Para el método getConnection del driver debe colocar el url la base de datos a la que se desea conectar,
             // el nombre del esquema que desea utilizar y la contraseña de dicho esquema.
-            return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:BDPRUEBA", "un", "123");
+            return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:BDProyecto", "ADM", "ADM");
         } catch (SQLException e) {
             return null;
         }
-    }*/
-    
-    
-    
+    }
+
+    /**
+     * Recibe una llamada parametrizada y la manda a ejecutar a la base de datos.
+     * @param call
+     * @return retorna el objeto de llamada.
+     */
+    private static CallableStatement insertData(CallableStatement call) {
+        try {
+            call.executeUpdate();
+            return call;
+        } catch (SQLException e) {
+            return null;
+        } 
+    }
+
+    /**
+     * Recibe una consulta parametrizada y la manda a ejecutar a la base de datos.
+     * @param call
+     * @return retorna el objeto de llamada.
+     */
+    private static CallableStatement queryData(CallableStatement call) {
+        try {
+            call.execute();
+            return call;
+        } catch (SQLException e) {
+            return null;
+        } 
+    }
 }
